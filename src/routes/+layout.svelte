d887864335bd4473a91a66dcbc57993f73a49dbe
imrulkayessifat <kayesimrul224@gmail.com>
2023-09-01 15:59:06 +0600

    add map

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from "svelte";
 	import Makeup from "./Makeup.svelte";
 	import Table from "./Table.svelte";
+	import Map from "./Map.svelte";
 	let currentRoute = "";
 
 	onMount(() => {
@@ -11,18 +12,32 @@
 </script>
 
 <div class="app">
-	<nav>
-		<div class="fixed top-2  right-11 z-[99]">
-			<button class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-rose-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp" on:click={() => (currentRoute = "/layout")}>Layout</button>
+	
+		<div class="fixed top-2 right-11 z-[99]">
+			<button
+				class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-rose-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+				on:click={() => (currentRoute = "/layout")}>Layout</button
+			>
 		</div>
-		<div class="fixed top-16  right-11 z-[99]">
-			<button class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-orange-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp" on:click={() => (currentRoute = "/chart")}>Chart</button>
+		<div class="fixed top-16 right-11 z-[99]">
+			<button
+				class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-orange-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+				on:click={() => (currentRoute = "/chart")}>Chart</button
+			>
 		</div>
-	</nav>
+		<div class="fixed bottom-10 right-11 z-[99]">
+			<button
+				class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-teal-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+				on:click={() => (currentRoute = "/map")}>Map</button
+			>
+		</div>
+	
 
 	{#if currentRoute === "/layout"}
 		<Makeup />
 	{:else if currentRoute === "/chart"}
 		<Table />
+	{:else if currentRoute === "/map"}
+		<Map />
 	{/if}
 </div>
---
+++ src/routes/Map.svelte
@@ -0,0 +1,49 @@
+<script>
+    import { onMount } from "svelte";
+    import "ol/ol.css";
+    import Map from "ol/Map";
+    import View from "ol/View";
+    import GeoJSON from "ol/format/GeoJSON";
+    import { Vector as VectorLayer } from "ol/layer";
+    import { Vector as VectorSource } from "ol/source";
+    import { Style, Fill, Stroke } from "ol/style";
+
+    function getGeoJSONStyle() {
+        return new Style({
+            fill: new Fill({
+                color: "rgba(0, 106, 78, 0.75)",
+            }),
+            stroke: new Stroke({
+                color: "rgba(0, 106, 78, 0.75)",
+                width: 2,
+            }),
+        });
+    }
+
+    let map;
+
+    onMount(() => {
+
+        map = new Map({
+            target: "map",
+            layers: [
+                new VectorLayer({
+                    source: new VectorSource({
+                        format: new GeoJSON(),
+                        url: "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson",
+                    }),
+                    style: getGeoJSONStyle(),
+                }),
+            ],
+            view: new View({
+                center: [0, 0],
+                zoom: 2,
+            }),
+        });
+    });
+</script>
+
+<div id="map" class="w-full h-screen" />
+
+<style>
+</style>
