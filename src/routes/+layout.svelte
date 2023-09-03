eabfc11f06b37e279fde63143f8e500008486e27
imrulkayessifat <kayesimrul224@gmail.com>
2023-09-03 19:11:16 +0600

    add particles.js effect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,42 +4,43 @@
 	import Table from "./Table.svelte";
 	import Map from "./Map.svelte";
 	import Lottie from "./Lottie.svelte";
+
 	let currentRoute = "";
-	onMount(() => {
+	onMount(async () => {
 		currentRoute = window.location.pathname;
 	});
 </script>
 
 <div class="app">
-		<div>
-			<div class="fixed top-2 right-11 z-[99]">
-				<button
-					class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-rose-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
-					on:click={() => (currentRoute = "/layout")}>Layout</button
-				>
-			</div>
-			<div class="fixed top-16 right-11 z-[99]">
-				<button
-					class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-orange-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
-					on:click={() => (currentRoute = "/chart")}>Chart</button
-				>
-			</div>
-			<div class="fixed bottom-10 right-11 z-[99]">
-				<button
-					class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-teal-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
-					on:click={() => (currentRoute = "/map")}>Map</button
-				>
-			</div>
+
+	<div>
+		<div class="fixed top-2 right-11 z-[99]">
+			<button
+				class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-rose-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+				on:click={() => (currentRoute = "/layout")}>Layout</button
+			>
 		</div>
-		
-		{#if currentRoute === "/layout"}
-			<Makeup />
-		{:else if currentRoute === "/chart"}
-			<Table />
-		{:else if currentRoute === "/map"}
-			<Map />
-		{:else}
-			<Lottie />
-		{/if}
-	
+		<div class="fixed top-16 right-11 z-[99]">
+			<button
+				class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-orange-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+				on:click={() => (currentRoute = "/chart")}>Chart</button
+			>
+		</div>
+		<div class="fixed bottom-10 right-11 z-[99]">
+			<button
+				class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-teal-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+				on:click={() => (currentRoute = "/map")}>Map</button
+			>
+		</div>
+	</div>
+
+	{#if currentRoute === "/layout"}
+		<Makeup />
+	{:else if currentRoute === "/chart"}
+		<Table />
+	{:else if currentRoute === "/map"}
+		<Map />
+	{:else}
+		<Lottie />
+	{/if}
 </div>
--- src/routes/CountryTable.svelte
+++ src/routes/CountryTable.svelte
@@ -3,6 +3,7 @@
     import { countryDataStore, fetchAndPopulateCountryData } from "../stores";
     import IoIosArrowDropleft from "svelte-icons/io/IoIosArrowDropleft.svelte";
     import IoIosArrowDropright from "svelte-icons/io/IoIosArrowDropright.svelte";
+
     let countries = [];
     let page = 0;
     let totalPages = [];
@@ -10,6 +11,7 @@
     let itemsPerPage = 10;
 
     $: currentPageRows = totalPages.length > 0 ? totalPages[page] : [];
+
     const paginate = (items) => {
         const pages = Math.ceil(items.length / itemsPerPage);
 
@@ -20,6 +22,7 @@
 
         totalPages = [...paginatedItems];
     };
+
     onMount(() => {
         fetchAndPopulateCountryData();
         countryDataStore.subscribe((data) => {
@@ -27,14 +30,17 @@
             paginate(countries);
         });
     });
+
     const setPage = (p) => {
         if (p >= 0 && p < totalPages.length) {
             page = p;
         }
     };
+    console.log(countries)
 </script>
 
-<div id="countyTable"
+<div
+    id="countyTable"
     class="overflow-x-auto row-span-3 grid grid-rows-6 lg:col-span-3 border-2 rounded-md bg-white m-2"
 >
     <table class="table row-span-5">
@@ -42,12 +48,13 @@
             <tr>
                 <th
                     class="px-5 py-3 border-b-2 border-gray-200 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
-                    >Name</th
+                    >Flag</th
                 >
                 <th
                     class="px-5 py-3 border-b-2 border-gray-200 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
-                    >Flag</th
                 >
+                    Name
+                </th>
                 <th
                     class="px-5 py-3 border-b-2 border-gray-200 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
                     >CIOC</th
@@ -93,7 +100,9 @@
                         >
                         <td
                             class="text-xs py-2 px-4 border-b border-gray-200 bg-white"
-                            ><span class="bg-slate-400 w-4 h-2">{country.unMember ? "YES" : "NO"}</span></td
+                            ><span class="bg-slate-400 w-4 h-2"
+                                >{country.unMember ? "YES" : "NO"}</span
+                            ></td
                         >
                         <td
                             class="text-xs py-2 px-4 border-b border-gray-200 bg-white"
@@ -119,7 +128,7 @@
             {/each}
         </tbody>
     </table>
-    <nav class="flex  row-span-1 justify-center items-center gap-8">
+    <nav class="flex row-span-1 justify-center items-center gap-8">
         <button
             type="button"
             class="btn-next-prev m-8 w-8 h-8"
@@ -133,13 +142,13 @@
             class="btn-next-prev m-8 w-8 h-8"
             on:click={() => setPage(page + 1)}
         >
-        <IoIosArrowDropright />
+            <IoIosArrowDropright />
         </button>
     </nav>
 </div>
 
 <style>
-    #countyTable{
+    #countyTable {
         height: 550px !important;
     }
 </style>
