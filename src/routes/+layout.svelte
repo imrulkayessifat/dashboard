1bdb6ab7870b4a375720af222a8f7997e18b9322
imrulkayessifat <kayesimrul224@gmail.com>
2023-09-04 07:17:53 +0600

    translate button left right

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,21 +14,21 @@
 <div class="app">
 
 	<div>
-		<div class="fixed top-2 right-11 z-[99]">
+		<div class="group fixed top-2 right-11 z-[99]">
 			<button
-				class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-rose-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+				class="transition duration-300 ease-in-out transform hover:translate-x-1.5 flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-rose-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
 				on:click={() => (currentRoute = "/layout")}>Layout</button
 			>
 		</div>
-		<div class="fixed top-16 right-11 z-[99]">
+		<div class="group fixed top-16 right-11 z-[99]">
 			<button
-				class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-orange-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+				class="transition duration-300 ease-in-out transform hover:-translate-x-1.5 flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-orange-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
 				on:click={() => (currentRoute = "/chart")}>Chart</button
 			>
 		</div>
-		<div class="fixed bottom-10 right-11 z-[99]">
+		<div class="group fixed bottom-10 right-11 z-[99]">
 			<button
-				class="flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-teal-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+				class="transition duration-300 ease-in-out transform hover:translate-x-1.5 flex h-12 w-16 cursor-pointer items-center justify-center rounded-sm bg-teal-400 text-white shadow-md transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
 				on:click={() => (currentRoute = "/map")}>Map</button
 			>
 		</div>
